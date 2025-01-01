6ef54b426e4c390aeebf1267f4c1a5d82735bf93
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-01-01 23:17:37 +0530

    added home page and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 
 export default function App() {
     return (
         <div className="app">
             <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
 }
