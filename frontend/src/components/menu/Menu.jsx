c84741ad3691b7280d9717c0fcf2c959e38aa79f
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-01-02 23:42:34 +0530

    added category component

---
+++ frontend/src/components/menu/Menu.jsx
@@ -0,0 +1,45 @@
+import { menu_list } from "../../assets/assets.js";
+
+import "./menu.css";
+
+export default function Menu({ category, setCategory }) {
+    return (
+        <div className="explore-menu" id="explore-menu">
+            <h1>Explore our menu</h1>
+
+            <p className="explore-menu-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+                fugit placeat enim dolorem ea alias quidem adipisci accusantium
+                debitis ullam?
+            </p>
+
+            <div className="explore-menu-list">
+                {menu_list.map((item, index) => {
+                    return (
+                        <div
+                            onClick={() =>
+                                setCategory((prev) =>
+                                    prev === item.menu_name
+                                        ? "All"
+                                        : item.menu_name
+                                )
+                            }
+                            key={index}
+                            className="explore-menu-list-item"
+                        >
+                            <img
+                                className={
+                                    category === item.menu_name ? "active" : ""
+                                }
+                                src={item.menu_image}
+                                alt=""
+                            />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    );
+                })}
+            </div>
+            <hr />
+        </div>
+    );
+}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+
 import Header from "../../components/header/Header";
+import Menu from "../../components/menu/Menu";
 
 import "./home.css";
 
 export default function Home() {
+    const [category, setCategory] = useState("all");
+
     return (
         <div>
             <Header />
+            <Menu category={category} setCategory={setCategory} />
         </div>
     );
 }
