86f7f0dfded5dc6e913872783b4fbeeb3663a54b
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-04-18 23:12:42 +0530

    added sidebar component

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,27 @@
+import { assets } from "../../assets/assets";
 import "./sidebar.css";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </div>
+
+        <div className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </div>
+
+        <div className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Orders</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
