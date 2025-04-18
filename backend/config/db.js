3a4195f83bb489fc1f170b878756efced9ea0a97
Arindam Dasgupta <arindamdasgupta6@gmail.com>
2025-04-18 21:08:54 +0530

    added mongodb connection

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  await mongoose
+    .connect("mongodb+srv://root:root12345@tomato.kf2fsb6.mongodb.net//tomato")
+    .then(() => {
+      console.log("DB Connected");
+    });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const port = 4000;
 
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API Working");
